e355a8ed0c854af5760f9be40861110bb8511d0c
andresfe6619 <andresfe6619@gmail.com>
2022-11-17 01:25:23 -0500

    heroku

--- Documents/desarrollo web/backEnd01-main/src/Rutas/Productos.js
+++ Documents/desarrollo web/backEnd01-main/src/Rutas/Productos.js
@@ -9,9 +9,8 @@ import {
 } from "../Controllers/route-controller-dao/productController.js";
 
 import checkAdmin from "../middlewares/chekAdmin.js";
-import { graphqlHTTP } from "express-graphql";
-import productSchema from "../graphql/graphl.js";
-import { productGraphl } from "../Controllers/graphql.js";
+
+
 
 import { checkAuthentication } from "../Controllers/route-controller-dao/users.js"
 
@@ -24,19 +23,6 @@ router.post("/agregar", newProduct);
 router.put("/Listado/:id", updateById);
 router.delete("/Listado/:id", deleteById);
 
-router.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: productSchema,
-    rootValue: {
-      getProd: productGraphl.getProduct,
-      getProds: productGraphl.getProducts,
-      createProd: productGraphl.createProduct,
-      updateProd: productGraphl.updateProduct,
-      deleteProd: productGraphl.deleteProduct,
-    },
-    graphiql: true,
-  })
-);
+
 
 export default router;
